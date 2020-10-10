94fdc8120db0ce06138dc95ff3a56b8eff8103af
PulkitSingh2008 <72497714+PulkitSingh2008@users.noreply.github.com>
2020-10-10 16:09:00 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,15 @@
-print("")
+print("Calculator")
+num1=float(input())
+operator=input()
+num2=float(input())
+
+if operator == "+":
+  print(num1+num2)
+elif operator == "*":
+  print(num1*num2)
+elif operator == "/":
+  print(num1/num2)
+elif operator == "-":
+  print(num1-num2)
+else:
+  print("Operater not found")
